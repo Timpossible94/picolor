e1e223b111734044a85881be9d1cda1eb0e83d55
Timpossible94 <117294140+Timpossible94@users.noreply.github.com>
2023-07-16 16:45:36 +0200

    Add files via upload

--- picolor/settings.py
+++ picolor/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-h6dhw1z1(swpw@d-5t4+6097fg3nhffc$)_o0ppv9#h-st(@1g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','.vercel.app', '.now.sh']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -76,12 +75,12 @@ WSGI_APPLICATION = 'picolor.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
 
 
 # Password validation
@@ -125,10 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
-
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = "/bilder/"
