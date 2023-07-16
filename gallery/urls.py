fa7007f012a17aa558041050b408b5f616f6a339
Timpossible94 <117294140+Timpossible94@users.noreply.github.com>
2023-07-16 15:48:36 +0200

    Add files via upload

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name = "home"),
     path('Galerie/', views.gallery, name = "gallery"),
     path('Kontakt/', views.contact, name = "contact")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- picolor/settings.py
+++ picolor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = "/bilder/"
 
